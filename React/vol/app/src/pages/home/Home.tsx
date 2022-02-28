7f3c3acb986476f2ba501e1a184bbe9192d5502a
Romain Kowalski <rkowalsk@z4r5p5.42lyon.fr>
2022-02-28 18:16:22 +0100

    started home page components

--- React/vol/app/src/pages/home/Home.tsx
+++ React/vol/app/src/pages/home/Home.tsx
@@ -1,9 +1,20 @@
+import "./Home.css"
+import SignInButton from  "../../components/SignInButton"
+
+function SignInClick ()
+{
+	alert("allo");
+}
 
 function Home() {
 	return (
-	  <h1>Hello World</h1>
+		<div id="homepage">
+			<header>
+				<h1>PONG</h1>
+			</header>
+			<button onClick={SignInClick}>Sign In</button>
+		</div>
 	);
-  }
-  
-  export default Home;
-  
+}
+
+export default Home;
