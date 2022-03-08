464f19658e65c40d77f81a52b84e25ffaac7259a
cfrancoi <corentinfrancois31@gmail.com>
2022-03-08 21:10:02 +0100

    Localstorage: user example with username

--- React/vol/app/src/components/ProfileSummary/ProfileSummary.tsx
+++ React/vol/app/src/components/ProfileSummary/ProfileSummary.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./ProfileSummary.css";
 import defaultLogo from "../../ressources/images/user-icon-0.png";
+import IUser from "../../interface/User";
+
+function parsUserData(userData: string | null) : IUser | null
+{
+	if (userData === null)
+		return null;
+	
+	return (JSON.parse(userData));
+}
 
 function ProfileSummary() {
+
+	const userData = localStorage.getItem('user');
+	const user : IUser | null = parsUserData(userData);
+
+	function getUserName() : string
+	{
+		if (user === null)
+			return ("default");
+		return (user.username);
+	}
+
 	return (
 		<aside id="profile">
 			<img src={defaultLogo} alt="truc" />
 			<div id="infos">
-				<p> {'> '} Username</p>
+				<p> {'> '} {getUserName()}</p>
 				<p> {'> '} Level</p>
 			</div>
 		</aside>
