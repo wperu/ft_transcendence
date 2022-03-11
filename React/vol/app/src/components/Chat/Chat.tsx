627864e54268fbf080921d07fb810a115a44dd9a
Romain Kowalski <rkowalsk@z4r8p5.42lyon.fr>
2022-03-11 18:22:07 +0100

    chat scrolling and msg text wrapping

--- React/vol/app/src/components/Chat/Chat.tsx
+++ React/vol/app/src/components/Chat/Chat.tsx
@@ -1,26 +1,39 @@
-import React, {KeyboardEvent} from "react";
+import React, {KeyboardEvent, useState, useEffect} from "react";
 import ChatMessage from "../ChatMessage/ChatMessage";
 import "./Chat.css";
 
 function Chat()
 {
+	const [messages, setMessages] = useState<JSX.Element[]>([]);
+	let msg_list_ref = React.createRef<HTMLDivElement>();
+	
 	function pressedSend(event: KeyboardEvent<HTMLInputElement>)
 	{
-		if (event.key === "Enter")
+		if (event.key === "Enter" && event.currentTarget.value.length > 0)
 		{
-			console.log("sending: " + event.currentTarget.value);
+			const new_msg =  <ChatMessage src_name="toi" content={event.currentTarget.value} time="12/34/56 à 12h34" />;
+			let new_msg_list :JSX.Element[] = [...messages, new_msg];
+			setMessages(new_msg_list);
+			
+			console.log("added: ", event.currentTarget.value);
+			console.log(messages.length);
 			event.currentTarget.value = '';
 		}
 	};
 
+	useEffect( () => 
+	{
+		if (msg_list_ref.current)
+		{
+			console.log("scrolling");
+			msg_list_ref.current.scrollTop = msg_list_ref.current.scrollHeight;
+		}
+	} );
+	
 	return (
 		<div id="chat">
-			<div id="messages_list">
-				<ChatMessage src_name="a" content="Lorem ipsum chepakoi" time="12/34/56 à 12h34" />
-				<ChatMessage src_name="someone" content="Lorem ipsum chepakoi" time="12/34/56 à 12h34" />
-				<ChatMessage src_name="someone" content="Lorem ipsum chepakoi" time="12/34/56 à 12h34" />
-				<ChatMessage src_name="someone" content="Lorem ipsum chepakoidgrnjkgdf hbfhdjsakbfhjeav ewdghsafjjmavdj fdakjfehafjkeragf erkfdgsafhdagsfkadjjg fhd sajkfdajk" time="12/34/56 à 12h34" />
-				<ChatMessage src_name="z" content="Lorem ipsum chepakoi" time="12/34/56 à 12h34" />
+			<div id="messages_list" ref={msg_list_ref}>
+				{messages}
 			</div>
 			<footer id="msg_footer">
 				<input type="text" id="message_input" placeholder="placeholder" onKeyPress={pressedSend}/>
