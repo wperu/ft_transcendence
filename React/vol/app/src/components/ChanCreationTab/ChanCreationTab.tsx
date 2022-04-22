bb306c490d148ccd4efbd6d4a9883bcde8b358c3
Romain Kowalski <rkowalsk@z2r1p2.42lyon.fr>
2022-04-22 14:49:16 +0200

    chan creation tab improvements

--- React/vol/app/src/components/ChanCreationTab/ChanCreationTab.tsx
+++ React/vol/app/src/components/ChanCreationTab/ChanCreationTab.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useDebugValue, useState } from "react";
 import { useChatContext } from "../Sidebar/ChatContext/ProvideChat";
 import "./ChanCreationTab.css";
 import { CreateRoom } from "../../Common/Dto/chat/room";
@@ -6,13 +6,27 @@ import { CreateRoom } from "../../Common/Dto/chat/room";
 function ChanCreationTab()
 {
 	const chatCtx = useChatContext();
-	function CreateChan(event: React.SyntheticEvent)
+
+	const	[is_protected, setProtected] = useState<boolean>(false);
+
+	function checkPasswordBox()
+	{
+		setProtected(true);
+	}
+
+	function changePasswordBox()
+	{
+		setProtected(!is_protected);
+	}
+
+	function createChan(event: React.SyntheticEvent)
 	{
 		event.preventDefault();
 		const target = event.target as typeof event.target & {
 			channel_name: {value: string};
 			channel_visibility: {value :string};
 			password: {value: string};
+			password_confirmation: {value: string};
 			is_protected: {value: boolean};
 		};
 		if (target.channel_name.value.length === 0)
@@ -23,15 +37,24 @@ function ChanCreationTab()
 		{
 			
 			var data: CreateRoom;
-			if (target.is_protected)
+			if (target.is_protected.value === true)
 			{		
-				
-				data =
+				if (target.password.value.length === 0)
+					alert("You can't set an empty password");
+				else
 				{
-					room_name: target.channel_name.value,
-					private_room: (target.channel_visibility.value === "private_chan"),
-					password: target.password.value,
-				};
+					data =
+					{
+						room_name: target.channel_name.value,
+						private_room: (target.channel_visibility.value === "private_chan"),
+						password: target.password.value,
+					};
+					chatCtx.socket.emit("CREATE_ROOM", data);
+					target.channel_name.value = '';
+					target.channel_visibility.value = '';
+					target.password.value = '';
+					target.is_protected.value = false;
+				}
 			}
 			else
 			{
@@ -40,17 +63,18 @@ function ChanCreationTab()
 					room_name: target.channel_name.value,
 					private_room: (target.channel_visibility.value === "private_chan"),
 				};
+				chatCtx.socket.emit("CREATE_ROOM", data);
+				target.channel_name.value = '';
+				target.channel_visibility.value = '';
+				target.password.value = '';
+				target.password_confirmation.value = '';
+				target.is_protected.value = false;
 			}
-			chatCtx.socket.emit("CREATE_ROOM", data);
-			target.channel_name.value = '';
-			target.channel_visibility.value = '';
-			target.password.value = '';
-			target.is_protected.value = false;
 		}
 	}
 
 	return (
-		<form id="create_chan" onSubmit={CreateChan}>
+		<form id="create_chan" onSubmit={createChan}>
 			<div id="visibility_options">
 				<input type="radio" name="channel_visibility" id="private_chan"
 					value="private_chan" />
@@ -64,9 +88,13 @@ function ChanCreationTab()
 			<div id="password_options">
 				<div>
 					Protéger par mot de passe
-					<input type="checkbox" id="protected" name="is_protected" />
+					<input type="checkbox" id="protected" checked={is_protected}
+						onChange={changePasswordBox} name="is_protected" />
 				</div>
-				<input type="text" name="password" placeholder="Mot de passe"/>
+				<input type="text" name="password" placeholder="Mot de passe"
+					onChange={checkPasswordBox}/>
+				<input type="text" name="password_confirmation"
+					placeholder="Confirmez le mot de passe"/>
 			</div>
 			<footer id="create_chan_validation">
 				<input id="create_chan_button" type="submit"
