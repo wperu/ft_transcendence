dd6de50bfa3ef9278d8066e68db4984c7fadb087
Romain Kowalski <rkowalsk@z2r1p2.42lyon.fr>
2022-05-10 16:21:22 +0200

    forgot dto

--- Common/Dto/FinishedGameDto.ts
+++ Common/Dto/FinishedGameDto.ts
@@ -1,8 +1,19 @@
-export interface FinishedGameDto
+export interface PostFinishedGameDto
 {
 	date: Date,
 	user_one_id: number;
 	user_two_id: number;
 	user_one_score: number;
 	user_two_score: number;
+}
+
+export interface GetFinishedGameDto
+{
+	date: Date,
+	id_one: number;
+	id_two: number;
+	username_one: string;
+	username_two: string;
+	score_one: number;
+	score_two: number;
 }
