a8558dfc43be5922cc7dd5a22923c0505de01941
William Peru <wperu@z3r12p5.42lyon.fr>
2022-05-11 18:40:00 +0200

    [nest]#todo test method in room.message.service

--- Nest/vol/app/src/room/room.message.service.ts
+++ Nest/vol/app/src/room/room.message.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ChatMessageEntity } from "src/entities/message.entity";
+import { User } from "src/entities/user.entity";
 import { Repository } from "typeorm";
 
 @Injectable()
@@ -19,25 +20,36 @@ export class ChatMessageService
 	/** //todo
 	 * rm all message of one room
 	 */
-	async rmMessageOfRoom()
+	async rmMessageOfRoom() : Promise<void>
 	{
-
+		this.msgRepo.clear();
 	}
 
 	/** //todo	Bonus
 	 * rm one message by owner/admin (moderation)
 	 */
-	async rmOneMessage()
+	async rmOneMessage(message:ChatMessageEntity) :Promise <void>
 	{
-
+		this.msgRepo.remove(message);
 	}
 
 	/** //todo
 	 * 
 	 */
-	async addMessage()
+	async addMessage(chatmessage: ChatMessageEntity, message: string, sender: User) : Promise<ChatMessageEntity | string>
 	{
+		
+		if(chatmessage === undefined)
+			return("no Chatmessage exist !");
+		
+		let chatmessageRel: ChatMessageEntity = new ChatMessageEntity();
+		
+		chatmessageRel.Content = message;
+		chatmessageRel.sender = sender;
 
+		await this.msgRepo.save(chatmessageRel);
+
+		return(chatmessage);
 	}
 
 	/**
@@ -48,9 +60,9 @@ export class ChatMessageService
 	/** //todo
 	 * fetch all message of one room
 	 */
-	async getAllMessageOf()
+	async getAllMessageOf(): Promise<ChatMessageEntity[]>
 	{
-
+		return await this.msgRepo.find();
 	}
 
 	
